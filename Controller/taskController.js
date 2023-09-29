f1a068a0ead9b6b44f060e8ade9e3a11eccaa275
n1tuneyi <youssefhammam77@gmail.com>
2023-09-30 00:06:02 +0300

    asdf

--- Controller/taskController.js
+++ Controller/taskController.js
@@ -49,11 +49,12 @@ exports.getTask = async (req, res, next) => {
           numTasks: { $sum: 1 },
           tasks: {
             // Push here without _id field
-            $push: {
-              name: "$name",
-              date: "$date",
-              completedBy: "$completedBy",
-            },
+            // $push: {
+            //   name: "$name",
+            //   date: "$date",
+            //   completedBy: "$completedBy",
+            // },
+            $push: "$$ROOT",
           },
         },
       },
