10cb2f5f721babf3c84043182a9651f3f9b30801
n1tuneyi <youssefhammam77@gmail.com>
2024-07-05 22:50:42 +0300

    Place authorization in websocket header instead of getting it from message

--- websocket.js
+++ websocket.js
@@ -8,13 +8,16 @@ function setupWebSocket(server) {
   io.on("connection", socket => {
     console.log("A user connected");
 
+    const token = socket.handshake.headers["authorization"];
+    socket.token = token;
+    
     socket.on("message", async msg => {
       // Broadcast the message to all connected clients except sender
       // socket.broadcast.emit("message", data);
 
       msg = JSON.parse(msg);
 
-      await messageService.sendMessage(msg.groupID, msg.token, msg.content);
+      await messageService.sendMessage(msg.groupID, socket.token, msg.content);
 
       // // Broadcast the message to all clients including the sender
       io.emit(msg.groupID, msg.content);
