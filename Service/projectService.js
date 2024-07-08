725c31605d2ea2480eba962c29596ccc1f476eb5
n1tuneyi <youssefhammam77@gmail.com>
2024-07-08 12:00:49 +0300

    check for db concurrency

--- Service/projectService.js
+++ Service/projectService.js
@@ -5,17 +5,19 @@ const Group = require("../Model/groupModel");
 
 exports.createProject = async projectForm => {
   const group = await Group.findById(projectForm.group);
+
   if (!group) throw new AppError("Group not found", 404);
 
   const projectNames = (await Project.find({ group: group._id })).map(
     project => project.title
   );
+  console.log("project checked");
 
   if (projectNames.includes(projectForm.title))
     throw new AppError("Project name already exists", 400);
 
   const project = await Project.create(projectForm);
-
+  console.log("project created");
   await User.updateOne(
     { _id: projectForm.members[0] },
     {
